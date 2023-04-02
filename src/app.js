3e7d97544bd4bb00a7ef5a55b123163175b28815
Mk4Levi <120794789+Mk4Levi@users.noreply.github.com>
2023-04-02 06:55:27 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,19 +6,6 @@ const path = require("path");
 const hbs = require("hbs");
 const bcrypt = require("bcryptjs");
 
-// const mongoose = require("mongoose");
-
-// mongoose
-//   .connect(
-//     "mongodb+srv://Manthan6296:Mkp-6296@cluster0.yysjuox.mongodb.net/Registration_DB"
-//   )
-//   .then(function () {
-//     console.log("Successfully connected to MongoDB");
-//   })
-//   .catch((err) => {
-//     console.log("Could not connect to MongoDB" + err);
-//   });
-
 const app = express();
 
 require("./db/conn");
